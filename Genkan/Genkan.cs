68034bd4bcf94b85b0371d654436383532cadae0
Shane Grüling <shane.grueling@portalwelten.de>
2015-08-15 00:05:09 +0200

    Missing file from previous commit

---
+++ Genkan/Genkan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Genkan
+{
+    public class Tobira : ITobira
+    {
+        private IApiDiscoverer _apiDiscoverer;
+
+        public Tobira(IApiDiscoverer apiDiscoverer)
+        {
+            if (apiDiscoverer == null) throw new ArgumentNullException(nameof(apiDiscoverer));
+
+            _apiDiscoverer = apiDiscoverer;
+        }
+
+        public void Call(IRequest request, IResponse response)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            if (response == null) throw new ArgumentNullException(nameof(response));
+
+            var controllerName = request.GetControllerName();
+            var methodName = request.GetMethodName();
+
+            var invoke = _apiDiscoverer.Resolve(controllerName, methodName);
+            var result = invoke(request);
+
+            response.SetResult(result);
+        }
+    }
+}
